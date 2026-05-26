985449114a743b71dcf1fcf59a638f4558716a0b
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-26 19:19:53 -0300

    chore: update shareCode to ACIICGMHS4K

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=N939CCKFMTD&afp=tapi2' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=ACIICGMHS4K&afp=tapi2' });
     res.end();
   }
