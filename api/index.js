0d2a74a699d037a68551cbadd05ee55f1717a584
JCPrime01 <vanzokiller@gmail.com>
2026-05-29 10:34:09 -0300

    chore: shareCode SH9HMIP7DM9 -> 7Q80XA2JKDT (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=SH9HMIP7DM9&afp=tapi2' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=7Q80XA2JKDT&afp=tapi2' });
     res.end();
   }
