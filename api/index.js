60dc414f3305465eaf83f18040f0b8904f304f32
JCPrime01 <vanzokiller@gmail.com>
2026-06-04 21:45:36 -0300

    chore: shareCode H28801EV2AI -> 0MF9FQUQJPY (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=H28801EV2AI&afp=ta' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=0MF9FQUQJPY&afp=ta' });
     res.end();
   }
